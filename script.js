1c707e2d063ffb6df235818a200da7dee6b7fff7
Dev-13552 <devrathour110@gmail.com>
2025-09-13 20:54:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
-//complete this code
+
 class Animal {
-	constructor(species){
-		this.species = species
-	}
-	set species(value){
-		this._species = value
-	}
-	get species(){
-		return this._species
-	}
-	makeSound(){
-		console.log(`This ${this.species} makes a sound`)
-	}
+  constructor(species) {
+    this._species = species; // store species in a private-like property
+  }
+
+  // getter for species
+  get species() {
+    return this._species;
+  }
+
+  // method to make sound
+  makeSound() {
+    console.log(`The ${this._species} makes a sound`);
+  }
 }
 
-class Dog extends Animal {
-	constructor(species){
-	super(species)
-		
-	}
-	bark(){
-		console.log("woof")
-	}
-}
-
+// Cat class extends Animal
 class Cat extends Animal {
-		constructor(species){
-		super(species)
-		
-	}
-		purr(){
-		console.log("purr")
-	}
+  constructor(species) {
+    super(species); // call Animal's constructor
+  }
+
+  // specific Cat behavior
+  purr() {
+    console.log("purr");
+  }
+}
+
+// Dog class extends Animal
+class Dog extends Animal {
+  constructor(species) {
+    super(species); // call Animal's constructor
+  }
+
+  // specific Dog behavior
+  bark() {
+    console.log("woof");
+  }
 }
 
 // Do not change the code below this line
